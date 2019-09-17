c4b2b44b73f0dc45cef3ae6dc1422b20640a1030
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2019-09-17 11:15:17 +0300

    Set up basic functionality

--- src/Table.java
+++ src/Table.java
@@ -1,4 +1,33 @@
+import java.util.*;
+
 
 public class Table {
-
+	
+	//representing the flow of the discussion
+	LinkedList<Member> discussion; 
+	//keeping track of how often students contribute
+	HashMap<String, Integer> member_frequency;
+	ArrayList<Member> members;
+	
+	//tracking the state of the discussion
+	boolean isActive = false;
+	
+	
+	public Table() {
+		discussion = new LinkedList<Member>();
+		member_frequency = new HashMap<String, Integer>();
+		members = new ArrayList<Member>();
+	}
+	
+	public void addMember(Member m) {
+		members.add(m);
+		member_frequency.put(m.student_id, 0);
+	}
+	
+	public void removeMember(Member m) {
+		if(!isActive)
+			member_frequency.remove(m.student_id);
+	}
+	
+	
 }
