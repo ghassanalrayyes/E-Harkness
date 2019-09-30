c48bf60923b58112daaa30fd4eae84e99503a211
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2019-09-30 11:54:30 +0300

    Add seat distribution functionality

--- src/Table.java
+++ src/Table.java
@@ -11,10 +11,18 @@ public class Table {
 	//tracking the state of the discussion
 	boolean isActive = false;
 	
+	int max_members = 20;
+
+	//how the members are distributed
+	boolean[] seat_map = new boolean[max_members];
 	
-	public Table() {
+	int num_members;
+	
+	public Table(int num_members) {
 		discussion = new LinkedList<Member>();
 		members = new HashMap<String, Member>();
+		this.num_members = num_members; 
+		distributeMemebrs();
 	}
 	
 	public void addMember(Member m) {
@@ -28,6 +36,18 @@ public class Table {
 	
 	public void transitionTo(String studentID) {
 		discussion.add(members.get(studentID));
+	}
+	
+	private void distributeMemebrs() {
+		int spaces = 0;
+		if(max_members % num_members == 0) {
+			spaces = max_members / num_members;
+			for (int i = 0; i < seat_map.length; i+= spaces) {
+				seat_map[i] = true;
+			}
+		}else {
+			
+		}
 	}
 	
 	
