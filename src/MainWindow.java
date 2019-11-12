e36a7bed2850e21082e6458b1afae944e967d96d
adnanjabri <adnanjabri@192.168.0.180>
2019-11-12 22:13:23 +0200

    table w pics

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -67,12 +67,12 @@ public class MainWindow {
 		points[9]=new Point(770,125,false);
 		points[10]=new Point(795,200,false);
 		points[11]=new Point(770,275,false);
-		points[12]=new Point(700,330,false);
-		points[13]=new Point(600,375,false);
-		points[14]=new Point(500,395,false);
-		points[15]=new Point(400,395,false);
-		points[16]=new Point(300,395,false);
-		points[17]=new Point(200,375,false);
+		points[12]=new Point(700,325,false);
+		points[13]=new Point(600,370,false);
+		points[14]=new Point(500,390,false);
+		points[15]=new Point(400,390,false);
+		points[16]=new Point(300,390,false);
+		points[17]=new Point(200,370,false);
 		points[18]=new Point(100,330,false);
 		points[19]=new Point(30,275,false);
 		
@@ -106,7 +106,7 @@ public class MainWindow {
 				g.setColor(Color.BLACK);
 				for(int i=0;i<20;i++) {
 					if(points[i].used()) {
-						g.fillOval(points[i].getX(),points[i].getY(),5,5);
+						g.fillOval(points[i].getX(),points[i].getY(),10,10);
 						
 					}
 				}
@@ -119,23 +119,113 @@ public class MainWindow {
 				System.out.println(e.getY());
 			}
 		});
+		
+		JLabel p1 = new JLabel("");
+		p1.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p1.setBounds(152, 295, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p1);
+		
+		JLabel p2 = new JLabel("");
+		p2.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p2.setBounds(180, 183, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p2);
+		
+		JLabel p3 = new JLabel("");
+		p3.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p3.setBounds(275, 100, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p3);
+		
+		JLabel p4 = new JLabel("");
+		p4.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p4.setBounds(367, 65, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p4);
+		
+		JLabel p5 = new JLabel("");
+		p5.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p5.setBounds(460, 45, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p5);
+		
+		JLabel p6 = new JLabel("");
+		p6.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p6.setBounds(738, 45, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p6);
+		
+		JLabel p7 = new JLabel("");
+		p7.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p7.setBounds(831, 65, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p7);
+		
+		JLabel p8 = new JLabel("");
+		p8.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p8.setBounds(923, 100, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p8);
+		
+		JLabel p9 = new JLabel("");
+		p9.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p9.setBounds(1018, 183, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p9);
+		
+		JLabel p10 = new JLabel("");
+		p10.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p10.setBounds(1046, 295, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p10);
+		
+		JLabel p11 = new JLabel("");
+		p11.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p11.setBounds(1018, 407, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p11);
+		
+		JLabel p12 = new JLabel("");
+		p12.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p12.setBounds(923, 490, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p12);
+		
+		JLabel p13 = new JLabel("");
+		p13.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p13.setBounds(831, 525, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p13);
+		
+		JLabel p14 = new JLabel("");
+		p14.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p14.setBounds(738, 545, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p14);
+		
+		JLabel p17 = new JLabel("");
+		p17.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p17.setBounds(460, 545, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p17);
+		
+		JLabel p18 = new JLabel("");
+		p18.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p18.setBounds(367, 525, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p18);
+		
+		JLabel p19 = new JLabel("");
+		p19.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p19.setBounds(275, 490, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p19);
+		
+		JLabel p20 = new JLabel("");
+		p20.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		p20.setBounds(180, 407, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(p20);
 		canvas.setBounds(240, 145, 800, 400);
 		frmHarknessDiscussion.getContentPane().add(canvas);
 		
-		JLabel lblNewLabel = new JLabel("");
-		lblNewLabel.setIcon(new ImageIcon("img/logo.png"));
-		lblNewLabel.setBounds(-12, 6, 232, 210);
-		frmHarknessDiscussion.getContentPane().add(lblNewLabel);
+		JLabel kengs = new JLabel("");
+		kengs.setIcon(new ImageIcon("img/logo.png"));
+		kengs.setBounds(1042, 6, 232, 210);
+		frmHarknessDiscussion.getContentPane().add(kengs);
 		
 		sID = new JTextField();
 		sID.setText("Student ID");
-		sID.setBounds(232, 6, 213, 26);
+		sID.setBounds(6, 6, 213, 26);
 		frmHarknessDiscussion.getContentPane().add(sID);
 		sID.setColumns(10);
 		
 		sP = new JTextField();
 		sP.setText("Position");
-		sP.setBounds(232, 37, 213, 26);
+		sP.setBounds(6, 31, 213, 26);
 		frmHarknessDiscussion.getContentPane().add(sP);
 		sP.setColumns(10);
 		
@@ -146,13 +236,22 @@ public class MainWindow {
 				
 				//you forgot to enable the point
 				points[Integer.parseInt(sP.getText())-1].draw = true;
-				
 				sChart=table.getSeatingChart();
 				canvas.repaint();
 			}
 		});
-		addM.setBounds(449, 6, 53, 57);
+		addM.setBounds(217, 6, 53, 51);
 		frmHarknessDiscussion.getContentPane().add(addM);
+		
+		JLabel label = new JLabel("");
+		label.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		label.setBounds(599, 551, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(label);
+		
+		JLabel label_1 = new JLabel("");
+		label_1.setIcon(new ImageIcon("/Users/adnanjabri/git/E-Harkness/img/test.jpg"));
+		label_1.setBounds(599, 39, 82, 100);
+		frmHarknessDiscussion.getContentPane().add(label_1);
 		frmHarknessDiscussion.setTitle("Harkness Discussion");
 		frmHarknessDiscussion.setBounds(100, 100, 1280, 720);
 		frmHarknessDiscussion.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
