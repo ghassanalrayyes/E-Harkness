94286345fc5a2ea400a738ff468c6410457049ed
Noredeen Al-Zu'bi <noredeenalzubi@gmail.com>
2019-12-16 12:41:18 +0200

    Add class models and functionality

---
+++ src/Class.java
@@ -0,0 +1,18 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class Class {
+	
+	String name = "";
+	
+	int max_members = 20;
+	
+	public HashMap<String, Member> members;
+	public String[] seat_chart = new String[max_members];
+	
+	
+	public Class(String class_name) {
+		name = class_name;
+	}
+
+}
--- src/ClassManager.java
+++ src/ClassManager.java
@@ -3,6 +3,7 @@ import java.awt.Dimension;
 import java.awt.EventQueue;
 
 import javax.swing.JFrame;
+import javax.swing.JOptionPane;
 import javax.swing.JTabbedPane;
 import javax.swing.JToolBar;
 import javax.swing.Icon;
@@ -12,6 +13,15 @@ import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
 import java.awt.Font;
 import javax.swing.JPanel;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.HashMap;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
 
 public class ClassManager {
 
@@ -19,7 +29,9 @@ public class ClassManager {
 
 	/**
 	 * Launch the application.
+	 * 
 	 */
+	
 	
 	
 	public static void main(String[] args) {
@@ -37,10 +49,44 @@ public class ClassManager {
 
 	/**
 	 * Create the application.
+	 * @throws IOException 
 	 */
+	
+	public void load() throws IOException {
+		try (Stream<Path> filePathStream=Files.walk(Paths.get("/home/you/Desktop"))) {
+		    filePathStream.forEach(filePath -> {
+		        if (Files.isRegularFile(filePath)) {
+		            try {
+						Files.lines(filePath);
+						
+						try (Stream<String> stream = Files.lines(Paths.get(filePath))) {
+
+							//1. filter line 3
+							//2. convert all content to upper case
+							//3. convert it into a List
+							list = stream
+									.filter(line -> !line.startsWith("line3"))
+									.map(String::toUpperCase)
+									.collect(Collectors.toList());
+						}
+						
+						
+						
+					} catch (IOException e) {
+						// TODO Auto-generated catch block
+						e.printStackTrace();
+					}
+		            
+		            
+		        }
+		    });
+		}
+	}
+	
 	public ClassManager() {
 		initialize();
 	}
+	
 
 	/**
 	 * Initialize the contents of the frame.
@@ -83,10 +129,30 @@ public class ClassManager {
 		
 		btnAddClass.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
+				//check if file exists
+				
+			    String class_name = JOptionPane.showInputDialog("Name of class:", "name");
+			    System.out.println(class_name);
+			    
+			    Class c = new Class(class_name);
+			    
+			    File myObj = new File("storage/Classes/" + class_name + ".txt");
+			    try {
+					if (myObj.createNewFile()) {
+						//load
+					}else {
+						 String class_name_trial2 = JOptionPane.showInputDialog(null, "A class with that name already exists. Pick a unique name:", "name", JOptionPane.WARNING_MESSAGE);
+					}
+				} catch (IOException e1) {
+					// TODO Auto-generated catch block
+					e1.printStackTrace();
+				}
+			    
+				
 			}
 		});
 		
-		btnAddClass.setBounds(588, 388, 91, 49);
+		btnAddClass.setBounds(581, 388, 91, 49);
 		frame.getContentPane().add(btnAddClass);
 		frame.setLocationRelativeTo(null);
 	}
